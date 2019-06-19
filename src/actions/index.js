686c7d1e041ce9f51f2eb075b3b5a3b333ccc931
George Adams <gadams@Georges-Air.local>
2019-06-19 18:01:32 -0500

    finished MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,20 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_ERROR = "FETCH_ERROR";
+
+export const getCharacters = () => dispatch => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get(`https://swapi.co/api/people/`)
+    .then(res => {
+      console.log(res.data, "my response!!!");
+      dispatch({ type: FETCH_SUCCESS, payload: res.data.results });
+    })
+    .catch(err => dispatch({ type: FETCH_ERROR }));
+};
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,20 +3,22 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
     // call our action
+    console.log("working yo");
+    this.props.getCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
+    // if (this.props.fetching) {
+    //   // return something here to indicate that you are fetching data
+    //   {
+    //     <h1>I'M FUCKING LOADING.... </h1>;
+    //   }
+    // }
     return (
       <div className="CharactersList_wrapper">
         <CharacterList characters={this.props.characters} />
@@ -27,9 +29,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  fetching: state.charsReducer.fetching,
+  erro: state.charsReducer.error,
+  fetched: state.charsReducer.fetched
+});
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { getCharacters }
 )(CharacterListView);
